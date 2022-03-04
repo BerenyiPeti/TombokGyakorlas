84e68c9cca708d80f3fedf322157423292c17f6e
BerenyiPeti <berenyipeti01@gmail.com>
2022-03-04 13:19:47 +0100

    bmi

---
+++ src/tombokgyakorlas/BMI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tombokgyakorlas;
+
+/**
+ *
+ * @author hallgato
+ */
+public class BMI {
+    public static void main(String[] args) {
+        
+    }
+    
+    private static void tulsulyosE(int szam){
+        String eredmeny = "";
+        if (szam < 20){
+            eredmeny = "kórosan sovány";
+        } else if (szam <= 25){
+            eredmeny = "normál";
+        } else if (szam < ){
+            
+        }
+    }
+    
+    
+}
